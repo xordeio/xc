1c8f10c0e6438affb400ab9975082199774bfc8b
XT <xt@xorde.co>
2021-09-23 16:28:30 -0400

    Added ipv6-disable

---
+++ cmd/ipv6-disable.sh
@@ -0,0 +1,1 @@
+printf "\n\nnet.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1\nnet.ipv6.conf.lo.disable_ipv6 = 1\nnet.ipv6.conf.tun0.disable_ipv6 = 1\n\n" >> /etc/sysctl.conf; echo "Please reboot"
