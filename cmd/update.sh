2e9efebc70f2eb861625d7ec09c9ec7dcfad7055
XT <xt@xorde.co>
2021-09-23 16:16:39 -0400

    Cache workarounds

--- cmd/update.sh
+++ cmd/update.sh
@@ -1,1 +1,1 @@
-curl -s https://raw.githubusercontent.com/xordeio/xc/main/xc > /usr/bin/xc && chmod +x /usr/bin/xc
+curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/xordeio/xc/main/xc?$(date +%s) > /usr/bin/xc && chmod +x /usr/bin/xc
