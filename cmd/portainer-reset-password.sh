229b50bfa3a72bef9679398e52ab30ae0870fd30
XT <73406141+xtovski@users.noreply.github.com>
2022-04-02 13:03:24 -0400

    Update portainer-reset-password.sh

--- cmd/portainer-reset-password.sh
+++ cmd/portainer-reset-password.sh
@@ -1,2 +1,2 @@
-# Description: Updates portainer to latest version.
+# Description: Resets Portainer password, new password will be shown in output
 docker stop portainer && docker run --rm -v portainer_data:/data portainer/helper-reset-password && docker start portainer
