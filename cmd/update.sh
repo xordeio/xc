8eec3525d22349b8960c419afdcca2af41ef5dfd
XT <73406141+xtovski@users.noreply.github.com>
2022-04-13 11:53:51 -0400

    Update update.sh

--- cmd/update.sh
+++ cmd/update.sh
@@ -1,2 +1,2 @@
 # Description: Updates xc to latest version
-curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/xordeio/xc/main/xc?$(date +%s) > /usr/bin/xc && chmod +x /usr/bin/xc
+sudo sh -c "curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/xordeio/xc/main/xc?$(date +%s) > /usr/bin/xc && chmod +x /usr/bin/xc"
